e52730c33906b05b642cd06599c6e297e0d0e2c7
unknown <tkfydtls464@gmail.com>
2022-11-08 13:56:58 +0900

    first one

---
+++ main.py
@@ -0,0 +1,209 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+import os
+import pandas as pd
+import numpy as np
+from tqdm.auto import tqdm
+import random
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+CFG = {
+    'EPOCHS': 30,
+    'LEARNING_RATE':1e-2,
+    'BATCH_SIZE':256,
+    'SEED':42
+}
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+
+seed_everything(CFG['SEED'])
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+categorical_features = ['COMPONENT_ARBITRARY', 'YEAR']
+# Inference(실제 진단 환경)에 사용하는 컬럼
+test_stage_features = ['COMPONENT_ARBITRARY', 'ANONYMOUS_1', 'YEAR' , 'ANONYMOUS_2', 'AG', 'CO', 'CR', 'CU', 'FE', 'H2O', 'MN', 'MO', 'NI', 'PQINDEX', 'TI', 'V', 'V40', 'ZN']
+
+train = train.fillna(0)
+test = test.fillna(0)
+
+all_X = train.drop(['ID', 'Y_LABEL'], axis = 1)
+all_y = train['Y_LABEL']
+
+test = test.drop(['ID'], axis = 1)
+
+train_X, val_X, train_y, val_y = train_test_split(all_X, all_y, test_size=0.2, random_state=CFG['SEED'], stratify=all_y)
+
+
+def get_values(value):
+    return value.values.reshape(-1, 1)
+
+
+for col in train_X.columns:
+    if col not in categorical_features:
+        scaler = StandardScaler()
+        train_X[col] = scaler.fit_transform(get_values(train_X[col]))
+        val_X[col] = scaler.transform(get_values(val_X[col]))
+        if col in test.columns:
+            test[col] = scaler.transform(get_values(test[col]))
+
+le = LabelEncoder()
+for col in categorical_features:
+    train_X[col] = le.fit_transform(train_X[col])
+    val_X[col] = le.transform(val_X[col])
+    if col in test.columns:
+        test[col] = le.transform(test[col])
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data_X, data_y, distillation=False):
+        super(CustomDataset, self).__init__()
+        self.data_X = data_X
+        self.data_y = data_y
+        self.distillation = distillation
+
+    def __len__(self):
+        return len(self.data_X)
+
+    def __getitem__(self, index):
+        if self.distillation:
+            # 지식 증류 학습 시
+            teacher_X = torch.Tensor(self.data_X.iloc[index])
+            student_X = torch.Tensor(self.data_X[test_stage_features].iloc[index])
+            y = self.data_y.values[index]
+            return teacher_X, student_X, y
+        else:
+            if self.data_y is None:
+                test_X = torch.Tensor(self.data_X.iloc[index])
+                return test_X
+            else:
+                teacher_X = torch.Tensor(self.data_X.iloc[index])
+                y = self.data_y.values[index]
+                return teacher_X, y
+
+train_dataset = CustomDataset(train_X, train_y, False)
+val_dataset = CustomDataset(val_X, val_y, False)
+train_loader = DataLoader(train_dataset, batch_size = 42, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size = 42, shuffle=False)
+
+
+class Teacher(nn.Module):
+    def __init__(self):
+        super(Teacher, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=52, out_features=256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=256, out_features=1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=1024, out_features=256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=256, out_features=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        output = self.classifier(x)
+        return output
+
+
+def train(model, optimizer, train_loader, val_loader, scheduler, device):
+    model.to(device)
+
+    best_score = 0
+    best_model = None
+    criterion = nn.BCELoss().to(device)
+
+    for epoch in range(CFG["EPOCHS"]):
+        train_loss = []
+
+        model.train()
+        for X, y in tqdm(train_loader):
+            X = X.float().to(device)
+            y = y.float().to(device)
+
+            optimizer.zero_grad()
+
+            y_pred = model(X)
+
+            loss = criterion(y_pred, y.reshape(-1, 1))
+            loss.backward()
+
+            optimizer.step()
+
+            train_loss.append(loss.item())
+
+        val_loss, val_score = validation_teacher(model, val_loader, criterion, device)
+        print(
+            f'Epoch [{epoch}], Train Loss : [{np.mean(train_loss) :.5f}] Val Loss : [{np.mean(val_loss) :.5f}] Val F1 Score : [{val_score:.5f}]')
+
+        if scheduler is not None:
+            scheduler.step(val_score)
+
+        if best_score < val_score:
+            best_model = model
+            best_score = val_score
+
+    return best_model
+
+
+def competition_metric(true, pred):
+    return f1_score(true, pred, average="macro")
+
+
+def validation_teacher(model, val_loader, criterion, device):
+    model.eval()
+
+    val_loss = []
+    pred_labels = []
+    true_labels = []
+    threshold = 0.35
+
+    with torch.no_grad():
+        for X, y in tqdm(val_loader):
+            X = X.float().to(device)
+            y = y.float().to(device)
+
+            model_pred = model(X.to(device))
+
+            loss = criterion(model_pred, y.reshape(-1, 1))
+            val_loss.append(loss.item())
+
+            model_pred = model_pred.squeeze(1).to('cpu')
+            pred_labels += model_pred.tolist()
+            true_labels += y.tolist()
+
+        pred_labels = np.where(np.array(pred_labels) > threshold, 1, 0)
+        val_f1 = competition_metric(true_labels, pred_labels)
+    return val_loss, val_f1
+
+model = Teacher()
+model.eval()
+optimizer = torch.optim.Adam(model.parameters(), lr=CFG['LEARNING_RATE'])
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=1, threshold_mode='abs',min_lr=1e-8, verbose=True)
+
+teacher_model = train(model, optimizer, train_loader, val_loader, scheduler, device)
